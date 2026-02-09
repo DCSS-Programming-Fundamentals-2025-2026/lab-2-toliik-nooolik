18eadcf59f72c4c87d184f6061d6709ce8735008
Chumak <nikitachumak07@gmail.com>
2026-02-09 14:06:16 +0200

    Core Done

---
+++ HangmanEngine.cs
@@ -0,0 +1,59 @@
+﻿public class HangmanEngine
+{
+    private string _targetWord;
+    private bool[] _revealed;
+    public int CurrentLives { get; private set; }
+    public int MaxLives { get; private set; }
+
+    public HangmanEngine(string word, int lives)
+    {
+        _targetWord = word.ToLower();
+        _revealed = new bool[_targetWord.Length];
+        MaxLives = lives;
+        CurrentLives = lives;
+    }
+
+    public bool MakeGuess(char letter)
+    {
+        letter = char.ToLower(letter);
+        bool hit = false;
+
+        for (int i = 0; i < _targetWord.Length; i++)
+        {
+            if (_targetWord[i] == letter && !_revealed[i])
+            {
+                _revealed[i] = true;
+                hit = true;
+            }
+        }
+
+        if (!hit)
+        {
+            CurrentLives--;
+        }
+
+        return hit;
+    }
+
+    public string GetDisplayWord()
+    {
+        char[] display = new char[_targetWord.Length];
+        for (int i = 0; i < _targetWord.Length; i++)
+        {
+            display[i] = _revealed[i] ? _targetWord[i] : '_';
+        }
+        return new string(display);
+    }
+
+    public bool IsWon()
+    {
+        for (int i = 0; i < _revealed.Length; i++)
+        {
+            if (!_revealed[i]) return false;
+        }
+        return true;
+    }
+
+    public bool IsGameOver() => CurrentLives <= 0 || IsWon();
+    public string GetSecretWord() => _targetWord;
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        WordProvider wordProvider = new WordProvider();
+        bool wantToPlay = true;
+
+        while (wantToPlay)
+        {
+
+            string secret = wordProvider.GetRandomWord();
+            HangmanEngine game = new HangmanEngine(secret, 6);
+
+
+            while (!game.IsGameOver())
+            {
+                GameInterface.Render(game);
+                char guess = GameInterface.AskLetter();
+                game.MakeGuess(guess);
+            }
+
+            GameInterface.Render(game);
+
+            if (game.IsWon())
+                Console.WriteLine("\n[Перемога!!!!!!] Красава!");
+            else
+                Console.WriteLine($"\n[Тебе повісили] Слово було: {game.GetSecretWord()}");
+
+
+            Console.WriteLine("\nЩе раз?  (y/n)");
+            string choice = Console.ReadLine().ToLower();
+            if (choice != "y" && choice != "n")
+            {
+                wantToPlay = false;
+            }
+        }
+
+        Console.WriteLine("Бувай");
+    }
+}
