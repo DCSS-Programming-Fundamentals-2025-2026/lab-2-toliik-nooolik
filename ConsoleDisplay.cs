1b2b49df476a153d97199f912bb8002e02bf6d17
pasaaa579-spec <pasaaa579@gmail.com>
2026-02-10 11:17:04 +0200

    Front

---
+++ ConsoleDisplay.cs
@@ -0,0 +1,21 @@
+﻿public static class GameInterface
+{
+    public static void Render(HangmanEngine engine)
+    {
+        Console.Clear();
+        Console.WriteLine("HANGMAN");
+        Console.WriteLine($"\nСлово: {engine.GetDisplayWord()}");
+        Console.WriteLine($"Залишилось спроб: {engine.CurrentLives} / {engine.MaxLives}");
+
+        
+        for (int i = 0; i < engine.CurrentLives; i++) Console.Write("❤");
+        Console.WriteLine("\n--------------------");
+    }
+
+    public static char AskLetter()
+    {
+        Console.Write("Введіть букву ");
+        string input = Console.ReadLine();
+        return (input != null && input.Length > 0) ? input[0] : ' ';
+    }
+}
